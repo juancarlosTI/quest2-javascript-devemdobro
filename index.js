05f7d550b9c97015855bdd55ffd4399609f6eb1d
Juan Carlos <juancarlosti@rede.ulbra.br>
2024-02-15 14:55:16 -0300

    quest-javascript medio

---
+++ index.js
@@ -0,0 +1,44 @@
+
+alert("Falta estilizar o texto '*campo obrigatorio' e o flex-start dos inputs");
+
+// ARRAY DE FORM'S
+
+let arrayPosts = [];
+
+function submit(){
+    //Informações do Form's
+    let inputs = document.querySelectorAll('input');
+    let erros = document.querySelectorAll('.error');
+    
+    let arrayItem = [];
+
+    for (let i = 0; i < inputs.length;i++){
+        let input = inputs[i];
+        let erro = erros[i];
+    
+
+        if (input.value.trim() === ''){
+            erro.classList.add('show');
+            input.classList.add('show-border');
+            input.classList.remove('report-ok');
+            console.log(`Vai ativar o input ${input.name} e o erro ${erro.textContent}`);
+        }
+        else {
+            arrayItem.push(input.value);
+            erro.classList.remove('show');
+            input.classList.add('report-ok');
+        }
+    }
+
+    console.log('ArrayItem :',arrayItem);
+
+    if (arrayItem.length !== 4){
+        console.log('Nada');
+    } else {
+        arrayPosts.push(arrayItem);
+    }
+
+    console.log('Array Posts: ',arrayPosts);
+}
+
+
